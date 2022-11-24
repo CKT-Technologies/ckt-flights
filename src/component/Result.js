7afacfc2865e0fbe325dbf2d2eada329eb1519b3
kvande97 <kylevandewiele97@gmail.com>
2022-11-24 11:09:19 -0600

    added transtion speed

--- src/component/Result.js
+++ src/component/Result.js
@@ -36,98 +36,43 @@ function Test(flightData) {
     <Row className="result-card mx-auto">
       <Col className="result">
         <Card.Body>
-          <Card.Title className="result-title">
-            Desitnation Route <FontAwesomeIcon icon={faArrowRightToBracket} />
-          </Card.Title>
-
+          <Card.Title className="result-title">Desitnation Route <FontAwesomeIcon icon={faArrowRightToBracket} /></Card.Title>
           <Row>
             <Col>
-              <Card.Text>
-                <FontAwesomeIcon icon={faPlaneDeparture} />{" "}
-                {result.departCodeDestination}{" "}
-              </Card.Text>
-
-              <Card.Text>
-                <FontAwesomeIcon icon={faCalendar} />{" "}
-                {result.departDateDestination}
-              </Card.Text>
-
-              <Card.Text>
-                <FontAwesomeIcon icon={faClock} />{" "}
-                {result.departTimeDestination}
-              </Card.Text>
+              <Card.Text><FontAwesomeIcon icon={faPlaneDeparture} />{" "}{result.departCodeDestination}{" "}</Card.Text>
+              <Card.Text><FontAwesomeIcon icon={faCalendar} />{" "}{result.departDateDestination}</Card.Text>
+              <Card.Text><FontAwesomeIcon icon={faClock} />{" "}{result.departTimeDestination} </Card.Text>
             </Col>
             <Col>
-              <Card.Text>
-                <FontAwesomeIcon icon={faPlaneArrival} />{" "}
-                {result.arriveCodeDestination}{" "}
-              </Card.Text>
-
-              <Card.Text>
-                <FontAwesomeIcon icon={faCalendar} />{" "}
-                {result.arriveDateDestination}
-              </Card.Text>
-
-              <Card.Text>
-                <FontAwesomeIcon icon={faClock} />{" "}
-                {result.arriveTimeDestination}
-              </Card.Text>
+              <Card.Text><FontAwesomeIcon icon={faPlaneArrival} />{" "}{result.arriveCodeDestination}{" "}</Card.Text>
+              <Card.Text><FontAwesomeIcon icon={faCalendar} />{" "}{result.arriveDateDestination}</Card.Text>
+              <Card.Text><FontAwesomeIcon icon={faClock} />{" "}{result.arriveTimeDestination}</Card.Text>
             </Col>
           </Row>
         </Card.Body>
       </Col>
       <Col className="result">
         <Card.Body>
-          <Card.Title className="result-title">
-            <FontAwesomeIcon icon={faArrowRightFromBracket} /> Return Route
-          </Card.Title>
-
+          <Card.Title className="result-title"><FontAwesomeIcon icon={faArrowRightFromBracket} /> Return Route</Card.Title>
           <Row>
             <Col>
-              <Card.Text>
-                <FontAwesomeIcon icon={faPlaneDeparture} />{" "}
-                {result.departCodeReturn}{" "}
-              </Card.Text>
-
-              <Card.Text>
-                <FontAwesomeIcon icon={faCalendar} /> {result.departDateReturn}
-              </Card.Text>
-
-              <Card.Text>
-                <FontAwesomeIcon icon={faClock} /> {result.departTimeReturn}
-              </Card.Text>
+              <Card.Text><FontAwesomeIcon icon={faPlaneDeparture} />{" "}{result.departCodeReturn}{" "}</Card.Text>
+              <Card.Text><FontAwesomeIcon icon={faCalendar} /> {result.departDateReturn}</Card.Text>
+              <Card.Text><FontAwesomeIcon icon={faClock} /> {result.departTimeReturn}</Card.Text>
             </Col>
             <Col>
-              <Card.Text>
-                <FontAwesomeIcon icon={faPlaneArrival} />{" "}
-                {result.arriveCodeReturn}{" "}
-              </Card.Text>
-
-              <Card.Text>
-                <FontAwesomeIcon icon={faCalendar} /> {result.arriveDateReturn}
-              </Card.Text>
-
-              <Card.Text>
-                <FontAwesomeIcon icon={faClock} /> {result.arriveTimeReturn}
-              </Card.Text>
+              <Card.Text><FontAwesomeIcon icon={faPlaneArrival} />{" "}{result.arriveCodeReturn}{" "}</Card.Text>
+              <Card.Text><FontAwesomeIcon icon={faCalendar} /> {result.arriveDateReturn}</Card.Text>
+              <Card.Text><FontAwesomeIcon icon={faClock} /> {result.arriveTimeReturn}</Card.Text>
             </Col>
           </Row>
         </Card.Body>
       </Col>
       <Col md={2} className="result">
         <Card.Body>
-          <Card.Title>
-            <FontAwesomeIcon icon={faMoneyBill1} /> Price
-          </Card.Title>
-          <Card.Text>
-            <FontAwesomeIcon icon={faDollarSign} /> {result.price}
-          </Card.Text>
-          <Button
-            className="nextButton btn-ckt"
-            onClick={console.log("Deets clicked")}
-          >
-            Details
-          </Button>
+          <Card.Title><FontAwesomeIcon icon={faMoneyBill1} /> Price</Card.Title>
+          <Card.Text><FontAwesomeIcon icon={faDollarSign} /> {result.price}</Card.Text>
+          <Button className="nextButton btn-ckt" onClick={console.log("Deets clicked")}>Details</Button>
         </Card.Body>
       </Col>
     </Row>
