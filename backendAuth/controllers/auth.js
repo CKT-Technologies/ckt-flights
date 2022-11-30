bb3f7c549da0658d144720660d460924fd19fefb
Christian <christiansmith2394@gmail.com>
2022-11-30 12:15:44 -0500

    Altered auth controller

--- backendAuth/controllers/auth.js
+++ backendAuth/controllers/auth.js
@@ -6,16 +6,7 @@ import User from "../models/User.js";
 
 export const register = async (req, res) => {
   try {
-    const {
-      firstName,
-      lastName,
-      email,
-      password,
-      picturePath,
-      friends,
-      location,
-      occupation,
-    } = req.body;
+    const { firstName, lastName, email, password, location } = req.body;
 
     const salt = await bcrypt.genSalt();
     const passwordHash = await bcrypt.hash(password, salt);
@@ -25,12 +16,7 @@ export const register = async (req, res) => {
       lastName,
       email,
       password: passwordHash,
-      picturePath,
-      friends,
       location,
-      occupation,
-      viewedProfile: Math.floor(Math.random() * 10000),
-      impressions: Math.floor(Math.random() * 10000),
     });
     const savedUser = await newUser.save();
     res.status(201).json(savedUser);
